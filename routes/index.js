053888c6fa7e3a7a828caefbaff232b548f9b806
edward880814 <edward880814@gmail.com>
2023-04-24 12:13:51 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
+// routes/index.js
 const express = require('express')
 const router = express.Router()
-
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')  // add this
 
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', users)  // add this
 
 module.exports = router
