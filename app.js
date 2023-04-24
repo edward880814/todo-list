dc1f36e7f13eff95bd65dcd82424bb6c54ca6f7d
edward880814 <edward880814@gmail.com>
2023-04-25 07:54:32 +0800

    feat: add user registeration feature

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
 
 const routes = require('./routes')
 require('./config/mongoose')
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
 
 const db = mongoose.connection
 
